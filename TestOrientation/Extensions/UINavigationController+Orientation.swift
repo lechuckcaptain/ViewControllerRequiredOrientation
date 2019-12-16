e11d4ff8da2bb3f93d192a732fcfd6d0c3b466cd
Marco Pagliari <lechuckcaptain@gmail.com>
2019-12-16 11:10:15 +0100

    Improved handling of modals

--- TestOrientation/Extensions/UINavigationController+Orientation.swift
+++ TestOrientation/Extensions/UINavigationController+Orientation.swift
@@ -4,19 +4,19 @@ import UIKit
 extension UINavigationController {
 
     override open var shouldAutorotate: Bool {
-        let sa = topViewController?.shouldAutorotate ?? super.shouldAutorotate
+        let sa = visibleViewController?.shouldAutorotate ?? topViewController?.shouldAutorotate ?? super.shouldAutorotate
         print("Orientation - '\(self.typeName)' shouldAutorotate: \(sa)")
         return sa
     }
 
     override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        let sio = topViewController?.supportedInterfaceOrientations ?? super.supportedInterfaceOrientations
+        let sio = visibleViewController?.supportedInterfaceOrientations ?? topViewController?.supportedInterfaceOrientations ?? super.supportedInterfaceOrientations
         print("Orientation - '\(self.typeName)' supportedInterfaceOrientations: \(sio)")
         return sio
     }
 
     override open var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
-        let v = topViewController?.preferredInterfaceOrientationForPresentation ?? super.preferredInterfaceOrientationForPresentation
+        let v = visibleViewController?.preferredInterfaceOrientationForPresentation ?? topViewController?.preferredInterfaceOrientationForPresentation ?? super.preferredInterfaceOrientationForPresentation
         print("Orientation - '\(self.typeName)' preferredInterfaceOrientationForPresentation: \(v)")
         return v
     }
